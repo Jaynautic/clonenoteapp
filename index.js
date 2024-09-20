f8527c91a7d60b5034c8b5b50647b9ca1f0713c8
Jaynautic <jaynauticstudios@gmail.com>
2024-09-20 16:16:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const backArrow = document.getElementById('backArrow');
+const dotMenu = document.getElementById('dotMenu');
+
+backArrow.addEventListener("click", function() {
+    alert("Wow");
+});
+
+dotMenu.addEventListener("click", function() {
+    alert("Woah");
+});
